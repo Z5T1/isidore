0df88b8e654653308323cdf58aca8889afbfc851
Scott Court <z5t1@z5t1.com>
2023-05-12 08:14:33 -0400

    Six setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,1 +1,5 @@
-export PYTHONPATH="$(realpath $(dirname $BASH_SOURCE[0]))/lib/src/isidore:$PYTHONPATH"
+# Source this file to set up your shell to use the Isidore libraries found
+# under ./lib/src, not the ones that pip installed. Useful when you want to
+# test changes without having to reinstall the libraries.
+export PYTHONPATH="$(realpath $(dirname $BASH_SOURCE[0]))/lib/src:$PYTHONPATH"
+
