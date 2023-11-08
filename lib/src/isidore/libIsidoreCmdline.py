e58d9c72c7de6cf104c7997d49d22568cc5ad8c8
Scott Court <z5t1@z5t1.com>
2023-11-08 17:25:16 -0500

    Simplify prompt printing

--- lib/src/isidore/libIsidoreCmdline.py
+++ lib/src/isidore/libIsidoreCmdline.py
@@ -56,10 +56,8 @@ class IsidoreCmdline:
             # Determine prompt
             name = self._isidore.getName()
             if sys.stdin.isatty():
-                display_prompt = \
-                    ( '[' + name + ']' if name else '' ) + \
-                    (' ' if  name and len(prompt) != 0 else '' ) + \
-                    ' '.join(prompt) + '> '
+                display_prompt = ' '.join( \
+                    ['[' + name + ']'] + prompt if name else prompt ) + '> '
             else:
                 display_prompt = ''
 
