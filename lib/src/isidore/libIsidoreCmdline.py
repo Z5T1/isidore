c861d3e55bbba5dc443a39f0f1238ba44328c0a8
Minor Keith <keithminork@gmail.com>
2023-11-27 11:49:03 -0500

    Added display_prompt to line variable

--- lib/src/isidore/libIsidoreCmdline.py
+++ lib/src/isidore/libIsidoreCmdline.py
@@ -75,8 +75,8 @@ class IsidoreCmdline:
 
             # Read input
             try:
-                #line = shlex.split(input(display_prompt))
-                line = shlex.split(gnureadline.get_line_buffer(display_prompt))
+                line = shlex.split(input(display_prompt))
+                #line = shlex.split(gnureadline.get_line_buffer(display_prompt))
             except EOFError:
                 print()
                 return
