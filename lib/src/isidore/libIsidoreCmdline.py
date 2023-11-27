f90206aa9f3cd8cffad1090e61a5d2d3c2b94f46
Minor Keith <keithminork@gmail.com>
2023-11-27 11:45:37 -0500

    Made a correction to a variable.

--- lib/src/isidore/libIsidoreCmdline.py
+++ lib/src/isidore/libIsidoreCmdline.py
@@ -76,7 +76,7 @@ class IsidoreCmdline:
             # Read input
             try:
                 #line = shlex.split(input(display_prompt))
-                line = shlex.split(readline.get_line_buffer())
+                line = shlex.split(gnureadline.get_line_buffer())
             except EOFError:
                 print()
                 return
